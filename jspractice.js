b1ff755896cddc9b155d0c2a8474054529d64990
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2025-12-03 07:33:04 +0000

    container config done

---
+++ jspractice.js
@@ -0,0 +1,22 @@
+// console.log("Hello");
+// console.log("I like pizza!");
+
+window.alert("This is an alert!");
+window.alert("I like pizza!");
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like Pizza!";
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log("age");
+console.log(`You are ${age} years old`);
+console.log(`The price: ${price}`);
+console.log(`The gpa of last ranked student: ${gpa}`)
+document.getElementById("okP").textContent = "Hey, I am Quamar"
+
+let college = "PW IOI";
+
+console.log(`Your college is ${college}`);
